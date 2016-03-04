c0e3bb45ca583f1a38120b5e5fefcd64d1a0dd68
Milan Grubnić <MilanG@aol.com>
2016-03-03 19:35:13 -0600

    changed to an array of objects and used repeat directive

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,30 +1,23 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.move = {
-        icon: 'img/move.jpg',
-        title: 'MOVE',
-        developer: 'Move, Inc.',
-        price: 0.99
-    };
-
-    $scope.shutterbugg = {
-        icon: 'img/shutterbugg.jpg',
-        title: 'Shutterbugg',
-        developer: 'Chico Dusty',
-        price: 2.99
-    };
-
-    $scope.gameboard = {
-        icon: 'img/gameboard.jpg',
-        title: 'Gameboard',
-        developer: 'Milan G.',
-        price: 1.99
-    };
-
-    $scope.forecast = {
+    $scope.apps = [{
         icon: 'img/forecast.jpg',
         title: 'Forecast',
         developer: 'Forecast',
         price: 1.99
-    };
-
+    }, {
+        icon: 'img/gameboard.jpg',
+        title: 'Gameboard',
+        developer: 'Milan G.',
+        price: 1.99
+    }, {
+        icon: 'img/shutterbugg.jpg',
+        title: 'Shutterbugg',
+        developer: 'Chico Dusty',
+        price: 2.99
+    }, {
+        icon: 'img/move.jpg',
+        title: 'MOVE',
+        developer: 'Move, Inc.',
+        price: 0.99
+    }]
 }]);
