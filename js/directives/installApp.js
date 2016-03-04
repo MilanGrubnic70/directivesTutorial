7e324eb034174e67b27de46436e4f795d12e3c4d
Milan Grubnić <MilanG@aol.com>
2016-03-03 20:42:14 -0600

    added link property to install app object

--- js/directives/installApp.js
+++ js/directives/installApp.js
@@ -1,7 +1,23 @@
 app.directive('installApp', function() {
     return {
-        scope: {},
         restrict: 'E',
-        templateUrl: 'js/directives/installApp.html'
-    };
+        scope: {},
+        templateUrl: 'js/directives/installApp.html',
+        link: function(scope, element, attrs) {
+            scope.buttonText = "Install",
+                scope.installed = false,
+
+                scope.download = function() {
+                    element.toggleClass('btn-active');
+                    if (scope.installed) {
+                        scope.buttonText = "Install";
+                        scope.installed = false;
+                    } else {
+                        scope.buttonText = "Uninstall";
+                        scope.installed = true;
+                    }
+                }
+        }
+
+    }
 });
